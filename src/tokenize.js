50c56713d1eaf6156949c7bd6cba8273fc7b0953
Davi Teodoro <davi.a.teodoro@gmail.com>
2022-05-10 22:46:28 -0300

    the leaf lang

--- src/tokenize.js
+++ src/tokenize.js
@@ -6,6 +6,58 @@ const {
   isQuote,
 } = require('./identify');
 
-const tokenize = () => {};
+const tokenize = (input) => {
+  const tokens = [];
+  let cursor = 0;
+
+  while (cursor < input.length) {
+    const char = input[cursor];
+
+    if (isParenthesis(char)) {
+      tokens.push({ type: 'Parenthesis', value: char });
+      cursor++;
+      continue;
+    }
+
+    if (isWhitespace(char)) {
+      cursor++;
+      continue;
+    }
+
+    if (isNumber(char)) {
+      const number = [];
+      while (isNumber(input[cursor])) {
+        number.push(input[cursor]);
+        cursor++;
+      }
+      tokens.push({ type: 'Number', value: parseInt(number.join('')) });
+      continue;
+    }
+
+    if (isLetter(char)) {
+      const name = [];
+      while (isLetter(input[cursor])) {
+        name.push(input[cursor]);
+        cursor++;
+      }
+      tokens.push({ type: 'Name', value: name.join('') });
+      continue;
+    }
+
+    if (isQuote(char)) {
+      const quote = [];
+      while (!isQuote(input[++cursor])) {
+        quote.push(input[cursor]);
+      }
+      tokens.push({ type: 'String', value: quote.join('') });
+      cursor++;
+      continue;
+    }
+
+    throw new Error(`Unexpected character: ${char}`);
+  }
+
+  return tokens;
+};
 
 module.exports = { tokenize };
